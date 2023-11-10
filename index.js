2229e7fb2798025823bcdaf081148a6997688a69
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2023-11-10 23:59:38 +0500

    index

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ let express = require("express");
 let app = express();
 let ejs = require("ejs");
 app.set("view engine", "ejs");
+let fs = require("fs")
+let filed=fs.readFileSync("text.txt");
 
 app.get("/", (req, res) => {
-  res.sendFile("/text.txt");
+  res.type("text/plain");
+  res.send(filed);
 });
 
 app.listen(9000, () => {
