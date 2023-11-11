52a031417951811b7b06dc992e364e0523756818
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2023-11-11 19:57:09 +0500

    f

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 let express = require("express");
 let app = express();
-
+let mongoose = require("mongoose");
 app.use(express.static(__dirname + "/views"));
 app.set("view engine", "ejs");
 
+mongoose.connect("mongodb://127.0.0.1:27017/user", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get("/", (req, res) => {
-  try {
-    res.render("home");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
+let testSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+});
+let testModel = mongoose.model("newTry", testSchema);
+app.get("/", async (req, res) => {
+  await testModel({
+    name: "ahmadTry",
+    age: 35,
+  }).save();
+  res.render("home");
 });
 
 app.listen(9000, () => {
