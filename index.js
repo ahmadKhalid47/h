3314c68dd006039c6ed7ac1e986d2f64dd8b3152
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2023-11-11 18:50:19 +0500

    e

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   try {
-    res.render("home");
+    res.send("home");
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
