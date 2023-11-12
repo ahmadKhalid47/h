62d9c189e0650eacc9ee53ffe649019bae4f24d0
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2023-11-12 19:40:25 +0500

    b

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 9000; // Use the PORT environment variable if available, or default to 9000
+const port = process.env.PORT || 9000;
 
-// Use environment variables for sensitive information
 const uri =
   process.env.MONGODB_URI ||
   "mongodb+srv://ahmadrazakhalid110:oOAi1LGYaD8vQN1y@cluster0.cr0kdsf.mongodb.net/";
@@ -14,22 +13,21 @@ app.set("view engine", "ejs");
 
 app.get("/", async (req, res) => {
   try {
-    // Connect to MongoDB using connection pooling
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    // Define the schema outside of the route handler
+    // Set the poolSize after connecting
+    mongoose.connection.set("poolSize", 5);
+
     const testSchema = new mongoose.Schema({
       name: String,
       age: Number,
     });
 
-    // Create the model outside of the route handler
     const TestModel = mongoose.model("newTry", testSchema);
 
-    // Use the model to save data
     await new TestModel({
       name: "ahmadTry",
       age: 35,
@@ -37,11 +35,9 @@ app.get("/", async (req, res) => {
 
     res.render("home");
   } catch (err) {
-    // Log the error for debugging purposes
     console.error(err);
     res.status(500).send("Internal Server Error");
   } finally {
-    // Close the MongoDB connection after the operation
     mongoose.connection.close();
   }
 });
