e87414ed947502cc463f214f0768e8bd8c78427c
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2023-11-13 01:05:12 +0500

    a

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ let mongoose = require("mongoose");
 app.use(express.static(__dirname + "/views"));
 app.set("view engine", "ejs");
 
-
 const uri =
   "mongodb+srv://ahmadrazakhalid110:oOAi1LGYaD8vQN1y@cluster0.cr0kdsf.mongodb.net/";
 
+await mongoose.connect(uri);
+let testSchema = await mongoose.Schema({
+  name: String,
+  age: Number,
+});
+let testModel = await mongoose.model("newTry", testSchema);
+
 app.get("/", (req, res) => {
-    res.render("home");
-    });
+  res.render("home");
+});
 
 app.get("/click", async (req, res) => {
-  await mongoose.connect(uri);
-    let testSchema = await mongoose.Schema({
-      name: String,
-      age: Number,
-    });
-    let testModel = await mongoose.model("newTry", testSchema);
-    await testModel({
-      name: "ahmadTry",
-      age: 35,
-    }).save();
-  res.redirect("/")
-})
+  await testModel({
+    name: "ahmadTry",
+    age: 35,
+  }).save();
+  res.redirect("/");
+});
 
 app.listen(9000, () => {
   console.log("ok");
