317cb19633ce343d6591c95f7ed1c6aff25b716f
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2023-11-12 19:35:17 +0500

    n

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-let express = require("express");
-let app = express();
-let mongoose = require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 9000; // Use the PORT environment variable if available, or default to 9000
+
+// Use environment variables for sensitive information
+const uri =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://ahmadrazakhalid110:oOAi1LGYaD8vQN1y@cluster0.cr0kdsf.mongodb.net/";
+
 app.use(express.static(__dirname + "/views"));
 app.set("view engine", "ejs");
 
-const uri =
-  "mongodb+srv://ahmadrazakhalid110:oOAi1LGYaD8vQN1y@cluster0.cr0kdsf.mongodb.net/";
-
 app.get("/", async (req, res) => {
   try {
-    await mongoose.connect(uri);
-    let testSchema = await mongoose.Schema({
+    // Connect to MongoDB using connection pooling
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // Define the schema outside of the route handler
+    const testSchema = new mongoose.Schema({
       name: String,
       age: Number,
     });
-    let testModel = await mongoose.model("newTry", testSchema);
-    await testModel({
+
+    // Create the model outside of the route handler
+    const TestModel = mongoose.model("newTry", testSchema);
+
+    // Use the model to save data
+    await new TestModel({
       name: "ahmadTry",
       age: 35,
     }).save();
+
     res.render("home");
   } catch (err) {
-    res.send(err);
+    // Log the error for debugging purposes
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  } finally {
+    // Close the MongoDB connection after the operation
+    mongoose.connection.close();
   }
 });
-app.listen(9000, () => {
-  console.log("ok");
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
